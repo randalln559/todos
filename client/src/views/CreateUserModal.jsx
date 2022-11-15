1f5454ae6075c56176d7c7d510a0e7732e5ea98c
randalln559 <115368266+randalln559@users.noreply.github.com>
2022-11-15 08:43:15 -0700

    Updating package.json

--- client/src/views/CreateUserModal.jsx
+++ client/src/views/CreateUserModal.jsx
@@ -1,124 +1,117 @@
-import './CreateUserModal.css';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import PswMessage from './PswMessage';
-import handleChange from '../actions/handleChange';
-import EmailTaken from './emailTaken';
+import "./CreateUserModal.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import PswMessage from "./PswMessage";
+import handleChange from "../actions/handleChange";
+import EmailTaken from "./emailTaken";
 
 export default function CreateUserModal({ handleModalChange }) {
+  const [inputs, setInputs] = useState({});
+  const [pswMessage, setPswMessage] = useState(false);
+  const [password, setPassword] = useState(false);
+  const [taken, setTaken] = useState(false);
 
-    const [inputs, setInputs] = useState({});
-    const [pswMessage, setPswMessage] = useState(false);
-    const [password, setPassword] = useState(false);
-    const [taken, setTaken] = useState(false);
+  let validPsw = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(inputs.psw);
 
-    let validPsw = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(inputs.psw);
+  const handlePswMessage = () => setPswMessage((p) => !p);
 
-    const handlePswMessage = () => setPswMessage(p => !p);
+  const passwordHandler = () => setPassword(true);
 
-    const passwordHandler = () => setPassword(true);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const changeHandler = (e) => setInputs(handleChange(inputs, e));
 
-    const changeHandler = (e) => setInputs(handleChange(inputs, e));
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post(`http://localhost:8080/api/create`, inputs)
-            .then(res => {
-                if (res.data.code === 11000) {
-                    setInputs(values => ({ ...values, email: '' }))
-                    setTaken(p => !p)
-                } else {
-                    navigate(`/toDoUser${res.data._id}`)
-                }
-            })
-            .catch(err => console.log(err))
-        handleModalChange(e);
-    }
-
-    const emailStyle = () => {
-        return {
-            'width': '100%',
-            'padding': '12px',
-            'border': '1px solid #ccc',
-            'borderRadius': '4px',
-            'boxSizing': 'borderBox',
-            'marginTop': '6px',
-            'marginBottom': '0',
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8080/api/create`, inputs)
+      .then((res) => {
+        if (res.data.code === 11000) {
+          setInputs((values) => ({ ...values, email: "" }));
+          setTaken((p) => !p);
+        } else {
+          navigate(`/toDoUser${res.data._id}`);
         }
-    }
+      })
+      .catch((err) => console.log(err));
+    handleModalChange(e);
+  };
 
-    return (
-        <div className='userModal'>
-            <form className='animateMdl' onSubmit={handleSubmit}>
-                <div className='userMdlHeader'>
-                    <h2>Create Your Account</h2>
-                </div>
-                <div className='userLoginMdl'>
-                    <label htmlFor='userFirstName'>First Name</label>
-                    <input
-                        type='text'
-                        id='userFirstName'
-                        name='first'
-                        value={inputs.first || ''}
-                        onChange={changeHandler}
-                        required
-                    />
-                    <label htmlFor='userLastName'>Last Name</label>
-                    <input
-                        type='text'
-                        id='last'
-                        name='last'
-                        value={inputs.last || ''}
-                        onChange={changeHandler}
-                        required
-                    />
-                    <label htmlFor='username'>Email</label>
-                    <input
-                        type='email'
-                        id='email'
-                        name='email'
-                        style={taken ? emailStyle() : null}
-                        value={inputs.email || ''}
-                        onChange={changeHandler}
-                        onFocus={setTaken(p => p ? !p : null)}
-                        required
-                    />
-                    {taken && <EmailTaken />}
-                    <label htmlFor='psw'>Password</label>
-                    <input
-                        type='password'
-                        name='psw'
-                        id='psw'
-                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                        title='Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters'
-                        value={inputs.psw || ''}
-                        onChange={changeHandler}
-                        onFocus={handlePswMessage}
-                        onBlur={handlePswMessage}
-                        required
-                    />
-                    {
-                        pswMessage &&
-                        <PswMessage
-                            psw={inputs.psw || ''}
-                            password={password}
-                            passwordHandler={passwordHandler}
-                            validPsw={validPsw}
-                        />
-                    }
-                    {
-                        validPsw &&
-                        <input
-                            type='submit'
-                            value='Submit'
-                            title='submitUser'
-                        />
-                    }
-                </div>
-            </form>
+  const emailStyle = () => {
+    return {
+      width: "100%",
+      padding: "12px",
+      border: "1px solid #ccc",
+      borderRadius: "4px",
+      boxSizing: "borderBox",
+      marginTop: "6px",
+      marginBottom: "0",
+    };
+  };
+
+  return (
+    <div className="userModal">
+      <form className="animateMdl" onSubmit={handleSubmit}>
+        <div className="userMdlHeader">
+          <h2>Create Your Account</h2>
         </div>
-    )
-};
+        <div className="userLoginMdl">
+          <label htmlFor="userFirstName">First Name</label>
+          <input
+            type="text"
+            id="userFirstName"
+            name="first"
+            value={inputs.first || ""}
+            onChange={changeHandler}
+            required
+          />
+          <label htmlFor="userLastName">Last Name</label>
+          <input
+            type="text"
+            id="last"
+            name="last"
+            value={inputs.last || ""}
+            onChange={changeHandler}
+            required
+          />
+          <label htmlFor="username">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            style={taken ? emailStyle() : null}
+            value={inputs.email || ""}
+            onChange={changeHandler}
+            required
+          />
+          {taken && <EmailTaken />}
+          <label htmlFor="psw">Password</label>
+          <input
+            type="password"
+            name="psw"
+            id="psw"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+            value={inputs.psw || ""}
+            onChange={changeHandler}
+            onFocus={handlePswMessage}
+            onBlur={handlePswMessage}
+            required
+          />
+          {pswMessage && (
+            <PswMessage
+              psw={inputs.psw || ""}
+              password={password}
+              passwordHandler={passwordHandler}
+              validPsw={validPsw}
+            />
+          )}
+          {validPsw && (
+            <input type="submit" value="Submit" title="submitUser" />
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('./config/toDo.config');
+const PORT = process.env.PORT || 8080;
 
-app.use(express.json(), express.urlencoded({ extended: true }));
+require("./config/toDo.config");
+
 app.use(cors());
+app.use(express.json(), express.urlencoded({ extended: true }));
 
-const Routes = require('./routes/todo.routes');
+const Routes = require("./routes/todo.routes");
 
 Routes(app);
 
 const port = 8080;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${PORT}`));
